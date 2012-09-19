c62ca22b541dda416814edaa356878eb8d0c728c
Amin <amin.w3dev@gmail.com>
2012-09-19 14:56:39 +0430

    Changed theme font to ubuntu

--- themes/zhongguo/zhongguo.lua
+++ themes/zhongguo/zhongguo.lua
@@ -17,7 +17,8 @@ theme.wallpaper_cmd = { "" }
 
 
 -- {{{ Styles
-theme.font      = "Terminus 10"
+theme.font      = "Ubuntu 10"
+--theme.font      = "Terminus 10"
 
 -- {{{ Colors
 theme.fg_normal = "#DCDCCC"
